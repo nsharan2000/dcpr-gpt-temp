22ad40f1f862b2fe765b13c61bee040311d79362
sharan nagarajan <60562906+nsharan2000@users.noreply.github.com>
2023-12-10 11:57:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def disable_form():
 
 
 def main():
-    st.title("Assistants API UI")
+    st.title("DCPR GPT")
     user_msg = st.chat_input(
         "Message", on_submit=disable_form, disabled=st.session_state.in_progress
     )
